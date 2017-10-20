dbbe8c3fcb6eb85732385cc4f6b9b178d79e2dd1
kvnxiao <xlzqwerty1@gmail.com>
2017-10-20 18:59:51 -0400

    update dependencies

--- meirei-jda/src/main/kotlin/com/github/kvnxiao/discord/meirei/jda/command/ICommandParser.kt
+++ meirei-jda/src/main/kotlin/com/github/kvnxiao/discord/meirei/jda/command/ICommandParser.kt
@@ -1,3 +1,18 @@
+/*
+ *   Copyright (C) 2017 Ze Hao Xiao
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package com.github.kvnxiao.discord.meirei.jda.command
 
 import com.github.kvnxiao.discord.meirei.command.Command
@@ -6,7 +21,7 @@ import java.lang.reflect.Method
 interface ICommandParser {
 
     /**
-     * Parse [CommandAnn] annotations in a class and returns a list of commands created from those annotations.
+     * Parse [Command] annotations in a class and returns a list of commands created from those annotations.
      *
      * @param[instance] The instance object for which its class is to be parsed.
      * @return[List] The list of commands created after parsing.
@@ -14,7 +29,7 @@ interface ICommandParser {
     fun parseAnnotations(instance: Any): List<ICommand>
 
     /**
-     * Creates a command by parsing a single [CommandAnn] annotation, with its execution method set as the method
+     * Creates a command by parsing a single [Command] annotation, with its execution method set as the method
      * targeted by the annotation.
      *
      * @param[instance] The instance object for which its class is to be parsed.
